b98bcf8b80bae85bf372020cdcd5c316d24d0090
Siva_Sai <22je0483@iitism.ac.in>
2023-07-20 22:42:09 +0530

    Complete upto exercise 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...num) {
+    for(let i=0;i<num.length;i++){
+        for(let j=0;j<array.length;j++){
+            if(array[j]===num[i]){
+                array.splice(j,1);
+                break;
+            }
+        }
+    }
+    return array;
 };
+
+console.log(removeFromArray([1,2,3,4],3,4,5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
